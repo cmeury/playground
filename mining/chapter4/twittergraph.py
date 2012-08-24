b2a0bdd36779f28c8c99b98fc6d9372f7fa8aed3
Cedric Meury <cm@wurmlo.ch>
2012-08-24 09:01:19 +0200

    importing keys works

--- mining/chapter4/twittergraph.py
+++ mining/chapter4/twittergraph.py
@@ -1,10 +1,10 @@
 import twitter
 import json
-import secret
 
-print access_token
+# import keys from file which is excluded from git via .gitignore
+from secret import *
+
 screen_name = 'DerCed'
-
 t = twitter.Twitter(domain='api.twitter.com', api_version='1')
 response = t.users.show(screen_name=screen_name)
 print json.dumps(response, sort_keys=True, indent=4)
