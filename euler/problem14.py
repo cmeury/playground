49489de38ef4c6d31a58341736dc5977bde5cccd
Cedric Meury <cm@wurmlo.ch>
2012-08-28 08:20:11 +0200

    due to time constraints (train journey), not very elegant but working solution

--- euler/problem14.py
+++ euler/problem14.py
@@ -1,21 +1,29 @@
-list = []
 
 def collatz(start):
     """ n -> n/2 (n is even),n -> 3n + 1 (n is odd)"""
+    list = [start]
+    n = start
+    while n != 2:
+        if n % 2 == 0:
+            n /= 2
+        else:
+            n = (n * 3) + 1
+        list.append(n)
 
-    # anchoring
-    if start == 2:
-        list.append(2)
-        return 1
+    list.append(1)
+    return list
 
-    # even
-    if start % 2 == 0:
-        new_start = start / 2
-    else:
-        new_start = (start * 3) + 1
 
-    list.append(new_start)
-    return collatz(new_start)
+chains = []
+for i in range(2,100):
+    chains.append(collatz(i))
 
-collatz(13)
-print list
+max_length = 0
+max_starting_point = 0
+for chain in chains:
+    if len(chain) > max_length:
+        max_length = len(chain)
+        max_starting_point = chain[0]
+
+print max_length
+print max_starting_point
