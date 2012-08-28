93b26ea6cfdd3801e9829b9f12ad798291367fb8
Cedric Meury <cm@wurmlo.ch>
2012-08-28 22:02:02 +0200

    problem 14 nearly solved, but answer not correct. needs testing for boundary cases.

--- euler/problem14.py
+++ euler/problem14.py
@@ -1,29 +1,27 @@
 
 def collatz(start):
     """ n -> n/2 (n is even),n -> 3n + 1 (n is odd)"""
-    list = [start]
+    counter = 0
+#    list = [start]
     n = start
     while n != 2:
         if n % 2 == 0:
             n /= 2
         else:
             n = (n * 3) + 1
-        list.append(n)
+#        list.append(n)
+        counter += 1
 
-    list.append(1)
-    return list
+#    list.append(1)
+    counter += 1
+    return counter
 
 
-chains = []
-for i in range(2,100):
-    chains.append(collatz(i))
+maximum = 1000000
+chain_lengths = []
+for i in range(2,maximum):
+    if i % 1000 == 0:
+        print i
+    chain_lengths.append(collatz(i))
 
-max_length = 0
-max_starting_point = 0
-for chain in chains:
-    if len(chain) > max_length:
-        max_length = len(chain)
-        max_starting_point = chain[0]
-
-print max_length
-print max_starting_point
+print max(chain_lengths)
