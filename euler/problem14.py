ac27fecd35e3a871976507e67464050cb087d305
Cedric Meury <cm@wurmlo.ch>
2012-08-28 22:36:15 +0200

    problem 14 solved

--- euler/problem14.py
+++ euler/problem14.py
@@ -1,27 +1,19 @@
-
 def collatz(start):
     """ n -> n/2 (n is even),n -> 3n + 1 (n is odd)"""
-    counter = 0
-#    list = [start]
+    counter = 1
     n = start
     while n != 2:
         if n % 2 == 0:
             n /= 2
         else:
             n = (n * 3) + 1
-#        list.append(n)
         counter += 1
 
-#    list.append(1)
     counter += 1
     return counter
 
-
-maximum = 1000000
 chain_lengths = []
-for i in range(2,maximum):
-    if i % 1000 == 0:
-        print i
+for i in range(2,1000000):                                # will be from 2 to 999'999
     chain_lengths.append(collatz(i))
 
-print max(chain_lengths)
+print chain_lengths.index(max(chain_lengths)) + 2   # because we started from 2 and not from 0
