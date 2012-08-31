2569b51ed8db331aa8b9601d819ffed50b6ccc82
Cedric Meury <cm@wurmlo.ch>
2012-09-01 00:21:49 +0200

    seems to be correct but takes ages. what's wrong? don't get it.

--- euler/problem10.py
+++ euler/problem10.py
@@ -1,59 +1,39 @@
 #The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 #Find the sum of all the primes below two million.
-
-import sys
+from array import array
 import math
 
-def find_prime(n):
-	idx = int(n)
-	if idx < 1:
-		raise ValueError("argument needs to be at least: 1")
-	if idx == 1:
-		return 2
+def find_sum_of_primes_below(size):
+    """
+    Sieve of Eratosthenes
+    """
+    if size < 1:
+        raise ValueError("argument needs to be at least: 1")
+    if size == 1:
+        return [2]
 
-	primes = []
-	i = 2
-	while i < idx:
-		print str(i) + " - " + str(len(primes))		
-		if str(i)[-1:] in (0,2,4,6,8):
-			i += 1
-			continue
-		if(check_prime(i)):
-			primes.append(i)
-		i += 1
-	print primes
-	sum = 0
-	for j in primes:
-		sum += j
-	return sum
+    numbers = array('c','1' * (size+1))
 
-def check_prime(x):
-	for i in range(2,x,1):
-		if x % i == 0:
-			return False
-	return True
+    p = 2
+#    primes = []
+    sum = 0
+    while True:
+        sum += p
+        print p
+        for i in range(p+p, size, p):
+            numbers[i] = '0'
 
-def mod(n):
-	if n == 2:
-		return [2]
-	factors = []
-	i = 2
-	while i < n/2 + 1:
-		if n % i == 0:
-			div = n / i
-			factors.append(i)
-			factors.extend(mod(div))
-			break	
-		i += 1
-	if len(factors) == 0:
-		factors.append(n)
-	return factors
+        found = False
+        for i in range(p+1, size):
+            if numbers[i] == '1':
+                p = i
+                found = True
+                break
+        if not found:
+            break
 
+    return sum
 
-
-if len(sys.argv) < 2:
-	print "please supply an argument"
-	sys.exit()
-
-argument = sys.argv[1]
-print find_prime(argument) 
+if __name__ == '__main__':
+    print find_sum_of_primes_below(2000000)
+#    print sum(find_primes_below(2000000))
