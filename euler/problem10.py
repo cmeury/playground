3983f8e4a3ee0c2c39ac1ca05b3a108d5cacbed3
Cedric Meury <cm@wurmlo.ch>
2012-09-01 01:21:36 +0200

    also works in python now, thanks meli

--- euler/problem10.py
+++ euler/problem10.py
@@ -1,39 +1,13 @@
-#The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
-#Find the sum of all the primes below two million.
-from array import array
-import math
-
 def find_sum_of_primes_below(size):
-    """
-    Sieve of Eratosthenes
-    """
-    if size < 1:
-        raise ValueError("argument needs to be at least: 1")
-    if size == 1:
-        return [2]
-
-    numbers = array('c','1' * (size+1))
-
-    p = 2
-#    primes = []
+    numbers = [True] * (size+1)
     sum = 0
-    while True:
-        sum += p
-        print p
-        for i in range(p+p, size, p):
-            numbers[i] = '0'
-
-        found = False
-        for i in range(p+1, size):
-            if numbers[i] == '1':
-                p = i
-                found = True
-                break
-        if not found:
-            break
-
+    for i in range(2, size):
+        if numbers[i] == True:
+            sum += i
+            for j in range(i+i,size,i):
+                numbers[j] = False
+        i += i
     return sum
 
 if __name__ == '__main__':
     print find_sum_of_primes_below(2000000)
-#    print sum(find_primes_below(2000000))
