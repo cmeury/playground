78d8a8d83a0c72f56f20e00319d27d80f0adb19f
Cedric Meury <cm@wurmlo.ch>
2012-09-17 23:10:34 +0200

    hooray, it works

--- euler/problem12.py
+++ euler/problem12.py
@@ -1,12 +1,44 @@
-def triangle(x):
-    return sum(xrange(1,x+1))
+import sys
 
-def factors(x):
+# from problem 7
+def prime_factors(n):
+    if n == 2:
+        return [2]
     factors = []
-    for i in range(1,x+1):
-        if x % i == 0:
+    i = 2
+    while i < n/2 + 1:
+        if n % i == 0:
+            div = n / i
             factors.append(i)
+            factors.extend(prime_factors(div))
+            break
+        i += 1
+    if len(factors) == 0:
+        factors.append(n)
     return factors
 
-for i in range(1,8):
-    print str(triangle(i)) + " - " + str(factors(triangle(i)))
+def triangle(x):
+    return (x * (x + 1)) / 2
+
+def factors(x):
+    pf = prime_factors(x)
+    factor_counts = []
+    added = []
+    for f in pf:
+        if not f in added:
+            count = pf.count(f)
+            factor_counts.append([f,count])
+            added.append(f)
+
+    product = 1
+    for fc in factor_counts:
+        product *= (fc[1] + 1)
+    return product
+
+i = 0
+while True:
+    i += 1
+    t = triangle(i)
+    if factors(t) > 500:
+        print t
+        break
