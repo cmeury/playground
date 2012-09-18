4bda22476808a3b8c956c8f785688ab62d0ce53e
Cedric Meury <cm@wurmlo.ch>
2012-09-18 19:53:29 +0200

    some comments

--- euler/level0/problem24.py
+++ euler/level0/problem24.py
@@ -14,3 +14,25 @@ def perms(permutations, digits):
 
 perms(permutations, range(3))
 print permutations
+
+#1. Nimm die erste Zahl -> 0
+#2. Entferne die gewählte Zahl aus der Liste -> [1,2]
+#3. Nimm die erste Zahl -> 1
+#4. Entferne die gewählte Zahl aus der Liste -> [2]
+#5. Liste hat nur 1 Element
+#6. Füge das ganze zur Liste hinzu (012)
+#retourniere
+#7. Nimm die zweite Zahl -> 2
+#8. Entferne die gewählte Zahl aus der Liste -> [1]
+#9. Liste hat nur 1 Element
+#10. Füge das ganze zur Liste hinzu (021)
+#retourniere
+#11. Schleife fertig, retourniere
+#
+#perms(x)
+#wenn die liste nur ein element hat:
+#output liste
+#retourniere
+#iteriere über die liste x:
+#neue_liste := Entferne die Zahl aus der Liste x
+#perms(neue_liste)
