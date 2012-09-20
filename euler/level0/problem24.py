cf6b2f381234d72d32c9580b0c6dbf8281d71c13
Cedric Meury <cm@wurmlo.ch>
2012-09-20 19:41:04 +0200

    finally works with 0,1,2. but really cumbersome.

--- euler/level0/problem24.py
+++ euler/level0/problem24.py
@@ -1,38 +1,20 @@
-permutations = []
+import copy
 
-def perms(permutations, digits):
-    if len(digits) == 1:
-        print permutations.extend(digits)
+results = []
+
+def perms(start, current):
+    if len(start) == 1:
+        new_result = copy.copy(current)
+        new_result.append(copy.copy(start[0]))
+        results.append(new_result)
         return
-    for digit in digits:
-        new_digits = digits[:]
-        new_digits.remove(digit)
-        permutations.append(digit)
-        perms(permutations[:], new_digits)
-    return
+    for i in start:
+        new_list = copy.copy(start)
+        new_list.remove(i)
+        new_current = copy.copy(current)
+        new_current.append(i)
+        perms(new_list, new_current)
 
+perms(range(3), [])
 
-perms(permutations, range(3))
-print permutations
-
-#1. Nimm die erste Zahl -> 0
-#2. Entferne die gewählte Zahl aus der Liste -> [1,2]
-#3. Nimm die erste Zahl -> 1
-#4. Entferne die gewählte Zahl aus der Liste -> [2]
-#5. Liste hat nur 1 Element
-#6. Füge das ganze zur Liste hinzu (012)
-#retourniere
-#7. Nimm die zweite Zahl -> 2
-#8. Entferne die gewählte Zahl aus der Liste -> [1]
-#9. Liste hat nur 1 Element
-#10. Füge das ganze zur Liste hinzu (021)
-#retourniere
-#11. Schleife fertig, retourniere
-#
-#perms(x)
-#wenn die liste nur ein element hat:
-#output liste
-#retourniere
-#iteriere über die liste x:
-#neue_liste := Entferne die Zahl aus der Liste x
-#perms(neue_liste)
+print sorted(results)
