8043955b5d08a556127dc97bc9694726e84f119e
Cedric Meury <cm@wurmlo.ch>
2012-09-20 22:31:12 +0200

    solved problem 21 and achieved level 1, yay

--- euler/level0/problem21.py
+++ euler/level0/problem21.py
@@ -3,25 +3,23 @@ import math
 def sum_of_proper_divisors(x):
     half = math.trunc(x / 2)
     divs = []
-    for i in xrange(half,0,-1):
+    for i in xrange(1,half+1,1):
         if x % i == 0:
             divs.append(i)
     return sum(divs)
 
+
 amicable_nums = set()
-for i in range(0,10000):
+for i in range(1,10000):
     if i in amicable_nums:
         continue
     current_sum = sum_of_proper_divisors(i)
-    viceversa = sum_of_proper_divisors(current_sum)
-    if i == viceversa:
-        amicable_nums.add(i)
-        amicable_nums.add(current_sum)
 
-print amicable_nums
+    # no amicable pair if e.g. sum(6) = 6
+    if i != current_sum:
+        viceversa = sum_of_proper_divisors(current_sum)
+        if i == viceversa:
+            amicable_nums.add(i)
+            amicable_nums.add(current_sum)
+
 print sum(amicable_nums)
-
-# 80568 incorrect
-# 40284 incorrect
-# 48942 incorrect
-# 23934 incorrect
