85ca4a1924f378ab69c73e35569a0afbcee670ea
Cedric Meury <cedric@meury.com>
2013-01-06 22:33:34 +0100

    questions asks to optimize algorithm, i decided to throw some more memory at it. -Xms1024m -Xmx14096m did the trick. lol!

---
+++ daaii/src/main/java/ch/wurmlo/week3/Question2.java
@@ -0,0 +1,40 @@
+package ch.wurmlo.week3;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class Question2 {
+
+    @SuppressWarnings("unused")
+    private static Logger log = LoggerFactory.getLogger(Question2.class);
+
+    public static void main(String[] args) {
+        KnapSackReader reader = null;
+        try {
+            reader = new KnapSackReader("knapsack2.txt");
+        } catch (IOException e) {
+            System.err.println("Could not read file");
+            System.exit(1);
+        }
+        int knapSackSize = reader.knapSackSize;
+        int numberOfItems = reader.numberOfItems;
+        int[] itemValues = reader.itemValues;
+        int[] itemWeights = reader.itemWeights;
+
+        int[][] dynamic = new int[numberOfItems][knapSackSize];
+        // initialize A[0,x] = 0 for all x = 0,1,2,..,w
+        for (int x = 0; x < knapSackSize; x++) {
+            dynamic[0][x] = 0;
+        }
+        for (int i = 1; i < numberOfItems; i++) {
+            for (int x = 0; x < knapSackSize; x++) {
+                int value1 = dynamic[i-1][x];
+                int value2 = itemWeights[i] > x ? 0 : dynamic[i-1][x-itemWeights[i]] + itemValues[i];
+                dynamic[i][x] = Math.max(value1, value2);
+            }
+        }
+        log.info("optimal solution: {}", dynamic[numberOfItems-1][knapSackSize-1]);
+    }
+}
